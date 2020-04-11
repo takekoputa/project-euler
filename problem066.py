667ad9af809fd17f29917404a15934def011032f
self-gautrang <self.gautrang@gmail.com>
2020-04-10 23:02:38 -0700

    Add problem 66
    Signed-off-by: self-gautrang <self.gautrang@gmail.com>

---
+++ problem066.py
@@ -0,0 +1,5 @@
+# Question: https://projecteuler.net/problem=66
+
+# Solution: https://oeis.org/A033313 (note that n is the position of non-square number)
+
+# https://mathworld.wolfram.com/PellEquation.html
