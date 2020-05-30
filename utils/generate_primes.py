1512b12e4f2f2225d5cdc7f93d4064fb73e10c94
self-gautrang <self.gautrang@gmail.com>
2020-05-30 01:40:34 -0700

    Change generating primes implementation to a more efficient one
    Signed-off-by: self-gautrang <self.gautrang@gmail.com>

--- utils/generate_primes.py
+++ utils/generate_primes.py
@@ -1,15 +1,17 @@
 import math
+import numpy as np
 
 N = 10**6
 
-primes = {i*2 + 1 for i in range(1, N//2)}
-primes.add(2)
-
-for i in range(3, int(math.sqrt(N)) + 1):
-    if not i in primes:
+is_prime = np.ones((N+1), dtype = np.bool)
+is_prime[0] = False
+is_prime[1] = False
+for i in range(2, int(math.sqrt(N)) + 1):
+    if not is_prime[i]:
         continue
-    for j in range(i*2, N+1, i):
-        primes.discard(j)
+    is_prime[2*i::i] = False
+
+primes = np.where(is_prime==True)[0]
 
 assert(len(primes) == 78498)
 
