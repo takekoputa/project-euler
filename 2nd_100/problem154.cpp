687b05ffb57ef2c4b3d14133654a80843323712f
Hoa Nguyen <hoanguyen@ucdavis.edu>
2020-09-05 20:37:44 -0700

    Update problem154.cpp

--- 2nd_100/problem154.cpp
+++ 2nd_100/problem154.cpp
@@ -1,5 +1,30 @@
 // Question: https://projecteuler.net/problem=154
 
+/*
+    . The coefficients of trinomial expansion follows multinomial theorem.
+    . The coefficients of (x+y+z)^N are in the form of multinomial(i, j, N-i-j) where i >= 0, j >= 0, N-i-j > =0.
+    . We have multinomial(i, j, N-i-j) = N! / i! / j! / (N-i-j)!.
+    . We want to find how many coefficients of the expansion are multiples of 10^12.
+    . Note that, for a number k, we can find the number of trailing digit 0's as follows,
+        Let p be the largest possible integer such that (2^p) | k. (p is called the order of the prime factor 2)
+        Let q be the largest possible integer such that (5^q) | k. (q is called the order of the prime factor 5)
+        The number of trailing zeros of k is min(p, q).
+    . Let m2[1..N] denote the order of the prime factor 2 of numbers in the range [1..N].
+        We have that, for every 2 number, there is one number that is divisible by 2.
+                      for every 2^2 number, there is one number that is divisible by 2^2.
+                      for every 2^3 number, there is one number that is divisible by 2^3.
+                      and so on
+        So, for each power of 2, namely 2^p, we can iterate through each multiple of 2^p, namely k*(2^p) and increase m2[k*(2^p)] by 1.
+        For a number that has the order p for prime factor 2, the number will be visited p times.
+        Therefore, m2[k] denote the order of the prime factor 2 of k.
+    . Let m2f[k] denote the order of the prime factor 2 of factorial k!.
+      We have that, k! = k * ((k-1)!).
+      So, m2f[k] = m2[k] + m2f[k-1].
+    . We can follow a similar procedure to find m5f.
+    . The number of trailing zeros of k! is m10f = min(m2f[k], m5f[k]).
+    . So, we can find the number of trailing zeros of multinomial(i, j, N-i-j) for all i+j <= N from the precalculated numbers.
+      The number of trailing zeros of multinomial(i, j, N-i-j) is min(m2f[N] - m2f[i] -m2f[j] - m2f[N-i-j], m5f[N] - m5f[i] -m5f[j] -m5f[N-i-j]).
+*/
 #include<iostream>
 #include<cstring>
 
@@ -54,17 +79,8 @@ int main()
             i64 n2 = m2f[N] - m2f[i] - m2f[j] - m2f[k];
             i64 n5 = m5f[N] - m5f[i] - m5f[j] - m5f[k];
             i64 n10 = min(n2, n5);
-            //i64 n_equals = (i==j) + (j==k) + (k==i);
             if (n10 >= 12)
-            {
-                //if (n_equals == 3) // not occur
-                //    ans += 1;
-                //else if (n_equals == 1)
-                //    ans += 3;
-                //else
-                //    ans += 6;
                 ans += 1;
-            }
         }
     }
     cout << ans << "\n";
